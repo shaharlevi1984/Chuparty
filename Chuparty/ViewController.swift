768b52cee523ad7604cc578038ab233f259a8558
Shahar Levi <shaharLe88@gmail.com>
2020-02-26 17:21:22 +0200

    Initial Commit

---
+++ Chuparty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chuparty
+//
+//  Created by Shahar Levi on 26/02/2020.
+//  Copyright © 2020 Shahar Levi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
