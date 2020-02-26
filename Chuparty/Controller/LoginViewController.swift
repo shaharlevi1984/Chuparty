0dc0b5209c64076f224b00e9ecfcec992dcab09e
Shahar Levi <shaharLe88@gmail.com>
2020-02-26 18:18:53 +0200

    Login Page Design

---
+++ Chuparty/Controller/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Chuparty
+//
+//  Created by Shahar Levi on 26/02/2020.
+//  Copyright © 2020 Shahar Levi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
